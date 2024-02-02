763976bbe7ce51d920a4bd41023e98a739921cbd
Henry Huang <hhuang@macbook.local>
2024-02-02 00:00:31 -0500

    start of my nvim setup

---
+++ lua/kodfish/init.lua
@@ -0,0 +1,3 @@
+require("kodfish.set")
+
+
---
+++ lua/kodfish/set.lua
@@ -0,0 +1,21 @@
+
+-- relative line
+vim.opt.nu = true
+vim.opt.rnu = true
+
+-- set tab to 4 spaces
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+-- how many spaces to tab when pressing tab
+vim.opt.shiftwidth = 4
+-- expand tab character to spaces
+vim.opt.expandtab = true
+
+-- auto indent when returning
+vim.opt.smartindent = true
+
+-- no line wrapping
+vim.opt.wrap = false
+
+-- line at 80
+vim.opt.colorcolumn = "80"
